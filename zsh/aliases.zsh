198e828ec0390561095e594bd15b7b584c43c967
Jaggy Gauran <poke@jag.gy>
2015-12-05 09:28:30 +0800

    Add a phinx shortcut.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -41,6 +41,7 @@ alias fuck='sudo $(fc -ln -1)'
 alias vm='ssh vagrant@127.0.0.1 -p 2222'
 alias gls='gls --color=auto --group-directories-first'
 alias cask='brew cask'
+alias px='phinx'
 
 
 ### Shortcuts
