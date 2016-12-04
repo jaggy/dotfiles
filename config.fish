432d69aff3af2d12fab1040ab7128218dfb7d891
Jaggy Gauran <i.am@jag.gy>
2016-12-04 18:18:03 +0800

    Migrate the fish aliase.

---
+++ config.fish
@@ -0,0 +1,17 @@
+alias vi "nvim"
+
+alias g 'git'
+alias ga 'git add'
+alias gaa 'git add -A'
+alias gb 'git branch'
+alias gc 'git commit -m'
+alias gco 'git checkout'
+alias gm 'git merge'
+alias gs 'git status'
+alias gp 'git push'
+alias gt 'git tag'
+alias gl 'git log'
+alias art "php artisan"
+alias plz "php please"
+alias cmp "composer"
+alias emacs 'emacs -q --load "$HOME/.dotfiles/emacs.d/init.el"'
