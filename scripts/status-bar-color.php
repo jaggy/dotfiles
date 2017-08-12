8b8022da2fb4160de5473629fba492981493a37a
Jaggy Gauran <i.am@jag.gy>
2017-08-12 20:24:23 +0800

    Made a bash script to change the tmux colors.

---
+++ scripts/status-bar-color.php
@@ -0,0 +1,36 @@
+<?php
+
+class Color {
+    function __construct($background, $highlight) {
+        $this->background = $background;
+        $this->highlight  = $highlight;}
+}
+
+class Tmux {
+    function __construct () {
+        $this->states = [
+            'normal'  => new Color($background = '#005f5f', $highlight = '#008787'),
+            'insert'  => new Color($background = '#5f0000', $highlight = '#a60000'),
+            'replace' => new Color($background = '#005f5f', $highlight = '#008787'),
+            'visual'  => new Color($background = '#005f5f', $highlight = '#008787'),
+        ];
+    }
+
+    function state ($state) {
+        if (! array_key_exists($state, $this->states)) {
+            throw new Exception;
+        }
+
+        $color = $this->states[$state];
+
+        exec("tmux set-window-option -t $(tmux display-message -p '#S') window-status-bg '{$color->background}'");
+        exec("tmux set-window-option -t $(tmux display-message -p '#S') status-bg '{$color->background}'");
+        exec("tmux set-window-option -t $(tmux display-message -p '#S') window-status-current-bg '{$color->highlight}'");
+
+        exec("tmux set-window-option -t $(tmux display-message -p '#S') status-fg white");
+        exec("tmux set-window-option -t $(tmux display-message -p '#S') window-status-fg white");
+        exec("tmux set-window-option -t $(tmux display-message -p '#S') window-status-current-fg white");
+    }
+}
+
+(new Tmux)->state($argv[1]);
