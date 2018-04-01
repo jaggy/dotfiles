a015c434c62bd683bfcb9c85d7ab1bc39bcdecdb
Jaggy Gauran <i.am@jag.gy>
2018-04-02 02:48:09 +0800

    Style the title bar!

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -244,7 +244,7 @@ values."
    dotspacemacs-loading-progress-bar t
    ;; If non nil the frame is fullscreen when Emacs starts up. (default nil)
    ;; (Emacs 24.4+ only)
-   dotspacemacs-fullscreen-at-startup t
+   dotspacemacs-fullscreen-at-startup nil
    ;; If non nil `spacemacs/toggle-fullscreen' will not use native fullscreen.
    ;; Use to disable fullscreen animations in OSX. (default nil)
    dotspacemacs-fullscreen-use-non-native nil
@@ -324,6 +324,7 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+  (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
   )
 
 (defun dotspacemacs/user-config ()
