777d6b3a954ebff4bb966504417cf03f0036ea26
Jaggy <jaggy@hey.com>
2024-02-14 07:56:30 +0800

    wip

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -51,7 +51,7 @@ alias txs='tmux new -s'
 alias txt='tmux attach -t'
 alias ct="colortest"
 alias so="source $HOME/.zshrc"
-alias pp="php vendor/bin/phpunit"
+alias pp="php ./vendor/bin/pest --parallel"
 alias hosts='sudo nvim /etc/hosts'        # Update the hosts file.
 
 alias ignore="echo -n \"*\n!.gitignore\" >> .gitignore"
@@ -123,3 +123,6 @@ function dssh() {
 # alias migrate='rake db:migrate VERSION=0 && rake db:migrate && ./Console/cake filling.inject && ./Console/cake setup'
 # alias migrate='phinx rollback -t 0 && phinx migrate'
 # alias migrate='art migrate:refresh --seed'
+
+alias php74="/opt/homebrew/Cellar/php@7.4/7.4.33_2/bin/php"
+alias ditto="/Users/jaggy/Code/ditto-cli/ditto"
