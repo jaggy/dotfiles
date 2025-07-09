6f94270c74af2035d3070f46914a263fdf5b04e2
Jaggy <jaggy@hey.com>
2025-07-09 18:29:22 +0800

    CS fixes

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -3,17 +3,12 @@ return {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     opts = function(_, opts)
-      -- Remove git branch
       opts.sections.lualine_b = {}
-
-      -- Clear entire right side
-      opts.sections.lualine_x = { "filename" } -- LSP, command, etc.
-      opts.sections.lualine_y = {} -- Filename as plain text
-      opts.sections.lualine_z = {} -- Clear far right
-
-      -- Remove filename from left side
       opts.sections.lualine_c = {}
+
+      opts.sections.lualine_x = { "filename" }
+      opts.sections.lualine_y = {}
+      opts.sections.lualine_z = {}
     end,
   },
 }
-
