91dd7ead4ac3a87841a50ddbf719151568f95144
Jaggy <jaggy@hey.com>
2025-07-09 12:28:54 +0800

    Use no-neck-pain

--- nvim/lua/plugins/zen-mode.lua
+++ nvim/lua/plugins/zen-mode.lua
@@ -1,54 +1,24 @@
 return {
   {
-    "folke/zen-mode.nvim",
+    "shortcuts/no-neck-pain.nvim",
     event = "VimEnter",
     opts = {
-      window = {
-        backdrop = 1,
-        width = 120,
-        height = 1,
-        options = {
-          signcolumn = "no",
-          number = false,
-          relativenumber = false,
-          cursorline = false,
-          cursorcolumn = false,
-          foldcolumn = "0",
-          list = false,
-        },
-      },
-      plugins = {
-        options = {
-          enabled = true,
-          ruler = false,
-          showcmd = false,
-          laststatus = 0,
-        },
-        twilight = { enabled = true },
-        gitsigns = { enabled = false },
-        tmux = { enabled = false },
-        kitty = {
-          enabled = false,
-          font = "+4",
-        },
+      width = 120,
+      autocmds = {
+        enableOnVimEnter = false,
       },
     },
     keys = {
-      { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" },
+      { "<leader>z", "<cmd>NoNeckPain<cr>", desc = "Toggle Centered Layout" },
     },
     config = function(_, opts)
-      require("zen-mode").setup(opts)
-      
-      local zen_enabled = false
-      
-      vim.api.nvim_create_autocmd({"BufEnter", "VimEnter"}, {
+      require("no-neck-pain").setup(opts)
+
+      vim.api.nvim_create_autocmd("VimEnter", {
         callback = function()
           vim.defer_fn(function()
-            if not zen_enabled and vim.bo.buftype == "" and vim.fn.expand("%") ~= "" then
-              zen_enabled = true
-              vim.cmd("ZenMode")
-            end
-          end, 50)
+            vim.cmd("NoNeckPain")
+          end, 500)
         end,
       })
     end,
