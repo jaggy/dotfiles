c1ea5d915a6db70203bd830dae5e8412901f71fc
Jaggy <jaggy@hey.com>
2025-07-27 04:23:41 +0800

    Add a helper to write tests faster

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -13,3 +13,61 @@ vim.keymap.set("n", "<leader>cp", function()
   vim.fn.setreg("+", relpath)
   vim.notify("Copied relative file path: " .. relpath)
 end, { desc = "Copy relative file path to clipboard" })
+
+vim.keymap.set("n", "<leader>tF", function()
+  local current_file = vim.fn.expand("%:t")
+  if not string.match(current_file, "%.php$") then
+    vim.notify("Not a PHP file!", vim.log.levels.ERROR)
+    return
+  end
+
+  local filename_without_ext = string.gsub(current_file, "%.php$", "")
+
+  vim.ui.input({ prompt = "Test name: " }, function(test_name)
+    if not test_name or test_name == "" then
+      return
+    end
+
+    if not string.match(test_name, "Test$") then
+      test_name = test_name .. "Test"
+    end
+
+    local root = require("lazyvim.util").root.get()
+    local test_dir = root .. "/tests/Feature/" .. filename_without_ext
+    local test_file = test_dir .. "/" .. test_name .. ".php"
+
+    vim.fn.mkdir(test_dir, "p")
+
+    if vim.fn.filereadable(test_file) == 1 then
+      vim.notify("Test file already exists: " .. test_file)
+      vim.cmd("edit " .. test_file)
+      return
+    end
+
+    local test_content = string.format(
+      [[<?php
+
+namespace Tests\Feature\%s;
+
+use Tests\Fixtures\ActingAs;
+
+uses(ActingAs\Jaggy::class);
+
+it('has a valid factory', function () {
+});
+
+function factory(array $attributes = [])
+{
+    return test()->post(route(''), [
+        ...$attributes,
+    ]);
+}
+]],
+      filename_without_ext
+    )
+
+    vim.fn.writefile(vim.split(test_content, "\n"), test_file)
+    vim.cmd("edit " .. test_file)
+    vim.notify("Created test file: " .. test_file)
+  end)
+end, { desc = "Create Feature Test" })
