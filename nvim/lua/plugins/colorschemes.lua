235d119db7146d50310b01fd1bdb546eadb0283c
Jaggy <jaggy@hey.com>
2025-07-28 19:51:43 +0800

    Monokai pro

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -38,6 +38,53 @@ return {
   },
   { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
   {
+    "loctvl842/monokai-pro.nvim",
+    config = function()
+      require("monokai-pro").setup({
+        transparent_background = false,
+        terminal_colors = true,
+        devicons = true,
+        styles = {
+          comment = { italic = true },
+          keyword = { italic = true },
+          type = { italic = true },
+          storageclass = { italic = true },
+          structure = { italic = true },
+          parameter = { italic = true },
+          annotation = { italic = true },
+          tag_attribute = { italic = true },
+        },
+        filter = "pro",
+        inc_search = "background",
+        background_clear = {},
+        plugins = {
+          bufferline = {
+            underline_selected = false,
+            underline_visible = false,
+          },
+          indent_blankline = {
+            context_highlight = "default",
+            context_start_underline = false,
+          },
+        },
+        override = function(c)
+          return {
+            Normal = { bg = "#000000" },
+            NormalFloat = { bg = "#000000" },
+            SignColumn = { bg = "#000000" },
+            LineNr = { bg = "#000000" },
+            CursorLineNr = { bg = "#000000" },
+            GitSignsAdd = { bg = "#000000" },
+            GitSignsChange = { bg = "#000000" },
+            GitSignsDelete = { bg = "#000000" },
+            Folded = { bg = "NONE" },
+            FoldColumn = { bg = "#000000" },
+          }
+        end,
+      })
+    end,
+  },
+  {
     "Shatur/neovim-ayu",
     lazy = false,
     priority = 1000,
@@ -56,7 +103,7 @@ return {
         update_interval = 1000,
         set_dark_mode = function()
           vim.api.nvim_set_option_value("background", "dark", {})
-          vim.cmd.colorscheme("github_dark_high_contrast")
+          vim.cmd.colorscheme("monokai-pro")
         end,
         set_light_mode = function()
           vim.api.nvim_set_option_value("background", "light", {})
